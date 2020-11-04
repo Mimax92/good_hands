aa66056323ab1b7085f29e5250e1875cf98670d1
Maciej <mijasmaciej@gmal.com>
2020-11-04 21:48:17 +0100

    models-fix

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.core.validators import RegexValidator
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Category(models.Model):
